22c82864859546caa055617800aff5a28c37f045
Simone <31547194+regi18@users.noreply.github.com>
2020-04-04 18:01:51 +0200

    fix: changed oder of insert
    foreign key constraint

--- ISTAT_parser.py
+++ ISTAT_parser.py
@@ -13,24 +13,9 @@ import codecs
 
 f = codecs.open("istat_data.sql","w+", "utf-8")
 
-f.write("-- province\n")
-
-# province
-with open('Codici statistici e denominazioni delle ripartizioni sovracomunali .csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=';')
-    line_count = 0
-    for row in csv_reader:
-        if line_count > 1:
-            if (row[14] != 'Unità non amministrativa' and row[14] != 'Provincia soppressa'):
-                # id, name, region (id),
-                f.write(f'INSERT INTO province VALUES(`{row[15]}`, `{row[12]}`, {int(row[5])});\n')
-        line_count += 1
-    print(f'Province: Processed {line_count} lines.')
 
 
-
-
-f.write("\n-- regions\n")
+f.write("-- regions\n")
 
 # regions
 with open('Codici statistici e denominazioni delle ripartizioni sovracomunali .csv') as csv_file:
@@ -47,6 +32,24 @@ with open('Codici statistici e denominazioni delle ripartizioni sovracomunali .c
     for region in list(dict.fromkeys(regions)):
         f.write(region)
 
+
+
+f.write("\n-- province\n")
+
+# province
+with open('Codici statistici e denominazioni delle ripartizioni sovracomunali .csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+    line_count = 0
+    for row in csv_reader:
+        if line_count > 1:
+            if (row[14] != 'Unità non amministrativa' and row[14] != 'Provincia soppressa'):
+                # id, name, region (id),
+                f.write(f'INSERT INTO province VALUES(`{row[15]}`, `{row[12]}`, {int(row[5])});\n')
+        line_count += 1
+    print(f'Province: Processed {line_count} lines.')
+
+
+
 # cities
 f.write("\n-- cities\n")
 
@@ -58,4 +61,3 @@ with open('Elenco-comuni-italiani.csv') as csv_file:
             f.write(f'INSERT INTO city VALUES({int(row[14])}, `{row[6]}`, `{row[13]}`);\n')
         line_count += 1
     print(f'Cities: Processed {line_count} lines.')
-
